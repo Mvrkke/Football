a87df18f6a8ff46be224ce4331178bb25969776d
Mvrkke <mvrkke@outlook.com>
2025-03-13 16:21:12 +0100

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Sharmarke Mohamed Hilowle on 13.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
